90539d2317d0c317ef184770070ef8719aba0d77
USER <USER@DESKTOP-1QMSGNG>
2025-12-07 14:43:36 +0300

    Add project files.

---
+++ FitnessManagementSystemWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessManagementSystemWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
